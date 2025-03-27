a9b201ca9bc82a50d6076dbb90662a2aed3bcc41
paexternal303 <paexternal303@oaisb.info>
2025-03-27 10:32:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-# Cleans expired sessions from DB
-def sessionCleaner():
-    tmp = get_expired_sessions()
+# Function: session_cleanup_worker
+# Description: Cleans expired sessions from the database
+def session_cleanup_worker():
+    expired_sessions = get_expired_sessions()
