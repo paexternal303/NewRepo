23a918fa4a4c6f0fdd65987be02750af44601d65
paexternal303 <paexternal303@oaisb.info>
2025-03-30 01:04:05 +0530

    Add author field and modify rendering

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ if __name__ == "__main__":
     post = BlogPost("Hello", "This is a post about blogging.")
     print(render_post(post))
 
+        def add_tag(self, tag):
+            self.tags.append(tag)
+            self.author = author
+
