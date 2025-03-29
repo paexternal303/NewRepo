e3c65252c16161bc96fc1245f7b9ee1532f6f737
paexternal303 <paexternal303@oaisb.info>
2025-03-30 00:58:48 +0530

    Add tagging system and modify views

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ if __name__ == "__main__":
     post = BlogPost("Hello", "This is a post about blogging.")
     print(render_post(post))
 
+        def add_tag(self, tag):
+            self.tags.append(tag)
+
